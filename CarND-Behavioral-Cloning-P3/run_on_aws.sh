2ee92e65969117ccfb19f4ffbc5a71e5a510f5dd
tachwali <y.tachwali@gmail.com>
2018-02-16 14:59:29 -0800

    update script

---
+++ CarND-Behavioral-Cloning-P3/run_on_aws.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+conda install -c menpo opencv
+conda install keras
+
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip
+unzip data.zip
+
+
+
+
