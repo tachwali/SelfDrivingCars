09b60fbabc59e0a7176dfeaa6e05e5fc7da798e1
tachwali <y.tachwali@gmail.com>
2018-02-16 23:09:24 -0800

    model.py clean up

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -18,15 +18,13 @@ train_observations, validation_observations = ut.get_train_validate_lines(csv_fi
 train_generator = ut.generate_data(train_observations)
 validation_generator = ut.generate_data(validation_observations)
 
-
-
 model = Sequential()
-# model.add(BatchNormalization())
 
 model.add(Lambda(lambda x: x/127.5 - 1.0,input_shape=(80,320,3)))
-# model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
 
-# what is the differece between relu and elu?
+## Use NVIDIA self driving car network architecture 
+## https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+
 ## convolutional layers
 model.add(Conv2D(24, 5, 5, subsample=(2,2), activation='relu'))  
 model.add(Conv2D(36, 5, 5, subsample=(2,2), activation='relu'))  
@@ -44,46 +42,18 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
 
-"""
-model.add(Flatten(input_shape=(160,320,3)))
-model.add(Dense(1))
-"""
-
-
-
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 ## fit the model
 model_path = "model.h5"
-#checkpoint = ModelCheckpoint(model_path, verbose=0, save_best_only=True)
-#callbacks_list = [checkpoint]
-
-#model.fit_generator(train_generator, samples_per_epoch=len(train_observations), validation_data=validation_generator,
-#                    nb_val_samples=len(validation_observations), nb_epoch=5, callbacks=callbacks_list)
 
 model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_observations), validation_data=validation_generator,
                     nb_val_samples=len(validation_observations), nb_epoch=5, verbose = 1)
 
+## save model and its history
 model.save('model.h5')
 
 pickle_file = 'model_history.p'
 with open(pickle_file, 'wb') as file_pi:
         pickle.dump(history_object.history, file_pi)
 
-
 print('Model history is cached in pickle file.')
-
-#To run: python drive.py model.h5
-
-"""
-model.add(Convolution2D(6,6,6,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,6,6,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
-"""
-
-
