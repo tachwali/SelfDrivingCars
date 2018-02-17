dc7c58983b5489e0b51621deb7ad58cf52e1fa4b
tachwali <y.tachwali@gmail.com>
2018-02-16 22:35:31 -0800

    adding more automation to aws execution

--- CarND-Behavioral-Cloning-P3/run_on_aws.sh
+++ CarND-Behavioral-Cloning-P3/run_on_aws.sh
@@ -5,6 +5,6 @@ conda install -c menpo opencv
 wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip
 unzip data.zip
 
+conda env export > aws_environment.yml
 
-
-
+python model.py
