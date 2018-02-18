40c050a9fde452c9d1d69b51f625de5b112eccd6
tachwali <y.tachwali@gmail.com>
2018-02-18 10:44:39 -0800

    add flipped images to data generator

--- CarND-Behavioral-Cloning-P3/data_util.py
+++ CarND-Behavioral-Cloning-P3/data_util.py
@@ -147,17 +147,36 @@ def generate_data(observations, batch_size=128):
                 center_image_path = './data/IMG/'+observation[0].split('/')[-1]
                 left_image_path = './data/IMG/'+observation[1].split('/')[-1]
                 right_image_path = './data/IMG/'+observation[2].split('/')[-1]
+		
+		center = preprocess_image(cv2.imread(center_image_path))
+		left = preprocess_image(cv2.imread(left_image_path))
+		right = preprocess_image(cv2.imread(right_image_path))
+		center_observation = float(observation[3])
+		left_observation = center_observation + steering_correction
+		right_observation = center_observation - steering_correction
+		
+                center_images.append(center)
+                steering_angle_center.append(center_observation))
 
-                center_images.append(preprocess_image(cv2.imread(center_image_path)))
-                steering_angle_center.append(float(observation[3]))
-
-                left_images.append(preprocess_image(cv2.imread(left_image_path)))
-                right_images.append(preprocess_image(cv2.imread(right_image_path)))
-
-                ## append the steering angles and correct for left/right images
-                steering_angle_left.append(float(observation[3]) + steering_correction)
-                steering_angle_right.append(float(observation[3]) - steering_correction)
-	    
+		## append the steering angles and correct for left/right images
+		left_images.append(left)
+                steering_angle_left.append(left_observation)
+                right_images.append(right)
+                steering_angle_right.append(right_observation)
+	    	
+		## add flipped images
+		center_flipped, center_observation_flipped = flip_observation(center, center_observation)
+		center_images.append(center_flipped)
+                steering_angle_center.append(center_observation_flipped)
+		
+		left_flipped, left_observation_flipped = flip_observation(left, left_observation)
+		left_images.append(left_flipped)
+                left_angle_center.append(left_observation_flipped)
+		
+		right_flipped, right_observation_flipped = flip_observation(right, right_observation)
+		right_images.append(right_flipped)
+                right_angle_center.append(right_observation_flipped)
+		
             images = center_images + left_images + right_images
             steering_angles = steering_angle_center + steering_angle_left + steering_angle_right
 
